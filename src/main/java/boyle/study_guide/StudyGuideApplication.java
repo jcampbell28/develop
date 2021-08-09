238386469ba42bf0322e0ff388588685ef39388d
Jess Boyle <jboyle@jesss-mbp.lan>
2021-08-09 18:53:11 -0400

    Adding Spring boot initializer files

---
+++ src/main/java/boyle/study_guide/StudyGuideApplication.java
@@ -0,0 +1,13 @@
+package boyle.study_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boyle/study_guide/StudyGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package boyle.study_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
